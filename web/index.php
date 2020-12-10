163c0b86a429b3e1b2a567489737fb47c8bc169b
aragon <andregaragon@gmail.com>
2020-12-09 22:29:36 -0300

    headers added

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+
     echo "Hello André";
-    // include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/router.php";
 
 ?>
