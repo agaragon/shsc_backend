3202b898f8060107f534455c1f380dc7cc7f7384
aragon <andregaragon@gmail.com>
2020-12-09 22:13:33 -0300

    phpRoutines added

--- web/phpRoutines/router.php
+++ web/phpRoutines/router.php
@@ -1,31 +1,33 @@
 <?php
-$action = $_POST['action'];
-switch ($action) {
-    case 'SAGA_REGISTRATION':
-        $tableName = 'user';
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/headersConfig.php";
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/registration.php";
-        registration($tableName);
-    break;
-    case 'SAGA_LOGIN':
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/signIn.php";
-        signIn();
-    break;
-    case "SAGA_SEND_IMAGE_INFO":
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/headersConfig.php";
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/dbConnection.php";
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/uploadFile.php";
-        uploadFile();
-    break;
-    case "SAGA_UPLOAD_IMAGE":
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/headersConfig.php";
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/uploadImage.php";
-        uploadImage();
-    break;
-    case "SAGA_GET_STORE_PHOTOS":
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/headersConfig.php";
-        include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/getUserPhotos.php";
-        getUserPhotos();
-    break;
-}
+
+echo "Hello world";
+// $action = $_POST['action'];
+// switch ($action) {
+//     case 'SAGA_REGISTRATION':
+//         $tableName = 'user';
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/headersConfig.php";
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/registration.php";
+//         registration($tableName);
+//     break;
+//     case 'SAGA_LOGIN':
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/signIn.php";
+//         signIn();
+//     break;
+//     case "SAGA_SEND_IMAGE_INFO":
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/headersConfig.php";
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/dbConnection.php";
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/uploadFile.php";
+//         uploadFile();
+//     break;
+//     case "SAGA_UPLOAD_IMAGE":
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/headersConfig.php";
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/uploadImage.php";
+//         uploadImage();
+//     break;
+//     case "SAGA_GET_STORE_PHOTOS":
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/headersConfig.php";
+//         include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/routes/getUserPhotos.php";
+//         getUserPhotos();
+//     break;
+// }
 ?>
