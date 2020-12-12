d3a46f7ee715c8d7d1e0ff08ea41ec62d3d6ffd2
aragon <andregaragon@gmail.com>
2020-12-12 11:00:49 -0300

    working on db registration

--- web/phpRoutines/routes/registration.php
+++ web/phpRoutines/routes/registration.php
@@ -12,7 +12,7 @@ function registration($tableName){
     $usersAddress = $_POST['address'];
     $usersPassword = $_POST['password'];
     $token = $_POST['token'];
-    $tableName = 'user';
+    $tableName = 'user_table';
     $conn = createDBConnection($dbServername,$dbUsername,$dbPassword,$dbName,$dbPort);
     $shouldCreateUser = 0;
     $shouldCreateUser += checkExistence($_POST['userName'],'username',$tableName,$conn);
