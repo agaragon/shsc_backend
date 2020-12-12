163434c8da5e80fe1be58996476ee4fbcaf03ff4
aragon <andregaragon@gmail.com>
2020-12-12 09:47:54 -0300

    connecting to db

--- web/phpRoutines/helpers/dbConnection.php
+++ web/phpRoutines/helpers/dbConnection.php
@@ -5,6 +5,7 @@ function createDBConnection($servername,$username,$password,$dbname){
   try{
     // $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
+    echo'You connected to the db';
     return $conn;
   } catch(PDOException $e){
     $error = array("status"=>"500",
