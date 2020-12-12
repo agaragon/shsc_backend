368f3e55a28fe508aeef109490acc1a81a700594
aragon <andregaragon@gmail.com>
2020-12-12 20:09:20 -0300

    working on db registration

--- web/phpRoutines/routes/getUserPhotos.php
+++ web/phpRoutines/routes/getUserPhotos.php
@@ -8,23 +8,15 @@
         $token = $_POST['token'];
         $connMain = createDBConnection($dbServername,$dbUsername,$dbPassword,$dbName,$dbPort);
         $sqlMain = "SELECT username FROM $dbUserTable WHERE token='".$_POST["token"]."'";
-        echo "Step1";
         $stmtMain = $connMain->prepare($sqlMain);
         $stmtMain->execute();
-        echo "Step2";
         $resultMain = $stmtMain->fetchAll();
         $userName = $resultMain[0][0];
-        echo $userName;
         $connImages = createDBConnection($dbServername,$dbUsername,$dbPassword,$dbName,$dbPort);
-        echo json_encode($connImages);
-        echo "step 2.5";
         $sqlImages = "SELECT imagename,imageaddress,imagedescription,price,brand FROM $dbTableImages WHERE username='$userName'";
-        echo $sqlImages;
         $stmtImages = $connImages->prepare($sqlImages);
         // $stmtImages = $connImages->exec($sqlImages);
-        echo "Step3";
         $stmtImages->execute();
-        echo "Step4";
         $resultImages = $stmtImages->fetchAll();
 
         echo json_encode($resultImages);  
