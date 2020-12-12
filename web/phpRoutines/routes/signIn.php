0540b36fe396528bb3fa106d87575ca68c9cd0e3
aragon <andregaragon@gmail.com>
2020-12-12 09:53:13 -0300

    connecting to db

--- web/phpRoutines/routes/signIn.php
+++ web/phpRoutines/routes/signIn.php
@@ -2,12 +2,13 @@
 include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/headersConfig.php";
 include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/dbConnection.php";
 include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/listUserFiles.php";
-
+$dbopts = parse_url(getenv('DATABASE_URL'));
 function signIn(){
     include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/helpers/dbCredentials.php";
     $token = $_POST['token'];
     $sql = "SELECT fullname,email,usersaddress,username FROM user WHERE token='".$_POST["token"]."'";
-    $conn = createDBConnection($dbServername,$dbUsername,$dbPassword,$dbName,$dbPort);
+    // $conn = createDBConnection($dbServername,$dbUsername,$dbPassword,$dbName,$dbPort);
+    $conn = createDBConnection($dbopts["host"],$dbopts["user"],$dbopts["pass"],ltrim($dbopts["path"],'/'),$dbopts["port"]);
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $result = $stmt->fetchAll();
