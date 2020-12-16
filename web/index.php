406b6dfc330b6fa5550a48e035a50a5e57f474fe
aragon <andregaragon@gmail.com>
2020-12-16 18:37:29 -0300

    registration working

--- web/index.php
+++ web/index.php
@@ -2,5 +2,5 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
 
-    include $_SERVER["DOCUMENT_ROOT"] . "/phpRoutines/router.php";
+    include $_SERVER["DOCUMENT_ROOT"] . "/web/phpRoutines/router.php";
 ?>
